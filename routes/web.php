b7b1c79dc06b39f7aa83d31f9b6ffde7b11164d8
idlpar <idlpar@yahoo.com>
2024-05-08 23:34:21 +0600

    Service Provider for Number Values

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/amar', function () {
+    return view('amar');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
